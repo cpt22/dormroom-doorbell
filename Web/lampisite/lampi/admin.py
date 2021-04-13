d53a20535388def486ac9c98f99b69dffc2262fd
Christian Tingle <ctingle18@gmail.com>
2021-04-13 12:08:43 -0400

    updated to support doorbell

--- Web/lampisite/lampi/admin.py
+++ Web/lampisite/lampi/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Lampi
+from .models import Lampi, Doorbell
 
 # Register your models here.
+admin.site.register(Doorbell)
 admin.site.register(Lampi)
