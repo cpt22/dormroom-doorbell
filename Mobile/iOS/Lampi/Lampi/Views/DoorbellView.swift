2ed173402a7ace4437e8d697006378851523bb44
Christian <ctingle18@gmail.com>
2021-05-09 12:33:16 -0400

    Added BTLE handling in doorbell

--- Mobile/iOS/Lampi/Lampi/Views/DoorbellView.swift
+++ Mobile/iOS/Lampi/Lampi/Views/DoorbellView.swift
@@ -17,44 +17,44 @@ struct DoorbellView: View {
     
     var body: some View {
         NavigationView{
-        VStack {
+            VStack {
                 VStack {
                     Text("WIFI Setup")
-                                    .padding()
+                        .padding()
                     HStack {
                         Text("SSID:")
-                                        .padding()
+                            .padding()
                         TextField(
-                                   "Wifi Name",
-                                    text: $wifiName)
+                            "Wifi Name",
+                            text: $wifiName)
                             .disableAutocorrection(true)
                     }
                     HStack {
                         Text("Password:")
-                                        .padding()
+                            .padding()
                         SecureField(
-                                   "Password",
-                                    text: $pass)
-                                   .disableAutocorrection(true)
+                            "Password",
+                            text: $pass)
+                            .disableAutocorrection(true)
                     }
                     
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Button(action: {}, label: {
                         Text("Save")
                     }).padding()
-
+                    
                 }
                 VStack {
                     Text("Association Code")
-                                    .padding()
+                        .padding()
                     HStack {
                         Text("#######")
-                       
+                        
                     }
                 }
                 
                 Spacer()
             }
-               .navigationBarTitle("Doorbell Setup")
+            .navigationBarTitle("Doorbell Setup")
         }
     }
 }
